9122e47b9b15d4d4d5dadad0c588e711ad08c7b5
SLOBY <szabolcs.maj@gmail.com>
2013-11-12 00:15:17 +0100

    Fixed manage.py to use appropriate dev settings. Dev settings also uses proper Db password

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -12,7 +12,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '{{ project_name }}',
-#        'USER': 'dbuser',
+        'USER': 'postgresql',
 #        'PASSWORD': 'dbpassword',
     }
 }
