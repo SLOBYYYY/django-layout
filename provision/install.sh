98d7621d55dfa4373f26c2d62b3390d2f51cfc0d
SLOBY <szabolcs.maj@gmail.com>
2013-11-11 23:13:54 +0100

    Fixed locale generation for virtual machine

--- provision/install.sh
+++ provision/install.sh
@@ -15,12 +15,12 @@ PGSQL_VERSION=9.1
 
 # Need to fix locale so that Postgres creates databases in UTF-8
 cp -p $PROJECT_DIR/provision/etc-bash.bashrc /etc/bash.bashrc
-locale-gen en_GB.UTF-8
+locale-gen hu_HU.UTF-8
 dpkg-reconfigure locales
 
-export LANGUAGE=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
+export LANGUAGE=hu_HU.UTF-8
+export LANG=hu_HU.UTF-8
+export LC_ALL=hu_HU.UTF-8
 
 # Install essential packages from Apt
 apt-get update -y
